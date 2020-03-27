a00df80d4e269b5e6f7d698c387bb98fb4d05b7f
2819786276@qq.com <woai13920884917>
2020-03-27 13:17:12 +0800

    add adaptsegnet model

---
+++ models/adaptsegnet_model.py
@@ -0,0 +1,134 @@
+import torch
+import itertools
+from .base_model import BaseModel
+from . import networks
+from torch import nn
+from util.image_pool import ImagePool
+import torch.nn.functional as F
+import util.loss as loss
+
+
+class AdaptsegnetModel(BaseModel):
+    @staticmethod
+    def modify_commandline_options(parser, is_train=True):
+        """Add new dataset-specific options, and rewrite default values for existing options.
+        """
+        parser.add_argument('--num_classes', type=int, default=2, help='for determining the class number')
+        parser.add_argument('--is_restore_from_imagenet', type=bool, default=True,
+                            help='for fcn determining whether or not to restore resnet50 from imagenet')
+        parser.add_argument('--resnet_weight_path', type=str, default='./resnetweight/',
+                            help='the path to renet_weight from imagenet')
+        if is_train:
+            parser.add_argument('--gan_mode', type=str, default='vanilla',
+                                help='the type of GAN objective.')
+        return parser
+
+    def __init__(self, opt):
+        BaseModel.__init__(self, opt)
+        self.loss_names = ["G", "D"]  # "loss_"
+        self.visual_names = ["imageB", "label", "prediction"]  # ""
+        if self.isTrain:
+            self.visual_names += ["imageA", "D_out_target1", "D_out_target2"]  # ""    , "fcreal_out"
+
+        self.model_names = ["AdaptSegnet"]  # "net"
+        if self.isTrain:
+            self.model_names += ["Model_D1", "Model_D2"]  # "net"
+
+        # 特征生成器
+        self.netAdaptSegnet = networks.AdaptSegnet(
+            is_restore_from_imagenet=opt.is_restore_from_imagenet,
+            resnet_weight_path=opt.resnet_weight_path,
+            num_classes=opt.num_classes,
+            gpu_ids=self.gpu_ids)
+
+        if self.isTrain:
+            # 输出空间判别器
+            self.netModel_D1 = networks.srdanet_ds(num_classes=opt.num_classes, gpu_ids=self.gpu_ids)
+            self.netModel_D2 = networks.srdanet_ds(num_classes=opt.num_classes, gpu_ids=self.gpu_ids)
+
+            # 语义分割损失
+            self.loss_function = nn.CrossEntropyLoss(ignore_index=255).to(self.device)
+
+            # 输出空间判别器损失
+            self.bce_loss = networks.GANLoss("vanilla").to(self.device)
+
+            # 优化器
+            self.optimizer = torch.optim.Adam(self.netAdaptSegnet.parameters(),
+                                              lr=opt.lr, betas=(opt.beta1, 0.999), weight_decay=0.0005)
+            self.optimizer_D = torch.optim.Adam(
+                itertools.chain(self.netModel_D1.parameters(), self.netModel_D2.parameters()),
+                lr=opt.lr_D, betas=(opt.beta1, 0.999), weight_decay=0.0005)
+            self.optimizers.append(self.optimizer)
+            self.optimizers.append(self.optimizer_D)
+
+    def set_input(self, input):
+        if self.isTrain:
+            self.imageA = input["A_img"].to(self.device)
+            self.label = input["A_label"].to(self.device)
+            self.imageB = input["B_img"].to(self.device)
+        else:
+            self.imageB = input["B_img"].to(self.device)
+            self.label = input["B_label"].to(self.device)
+
+    def forward(self):
+        if self.isTrain:
+            # iamgeA 通过 generator
+            self.pred1, self.pre = self.netAdaptSegnet(self.imageA)
+            self.prediction = self.pre.data.max(1)[1].unsqueeze(1)
+            self.pred1_cut, self.pred2_cut = self.pred1.detach(), self.pre.detach()
+
+            self.pred_target1, self.pred_target2 = self.netAdaptSegnet(self.imageB)
+            self.pred_target1_cut, self.pred_target2_cut = self.pred_target1.detach(), self.pred_target2.detach()
+
+            self.D_out_target1 = self.netModel_D1(F.softmax(self.pred_target1, dim=1))
+            self.D_out_target2 = self.netModel_D2(F.softmax(self.pred_target2, dim=1))
+
+        else:
+            _, self.pre = self.netAdaptSegnet(self.imageB)
+            self.prediction = self.pre.data.max(1)[1].unsqueeze(1)
+
+    def backward(self):
+        """计算两个损失"""
+        # 分割损失
+        loss_seg1 = self.loss_function(self.pred1, self.label.long().squeeze(1))
+        loss_seg2 = self.loss_function(self.pre, self.label.long().squeeze(1))
+
+        self.loss_cross_entropy = 0.1 * loss_seg1 + loss_seg2
+
+        # 输出空间对齐
+        loss_adv_target1 = self.bce_loss(self.D_out_target1, False)
+        loss_adv_target2 = self.bce_loss(self.D_out_target2, False)
+
+        self.loss_adv = 0.0002 * loss_adv_target1 + 0.001 * loss_adv_target2
+
+        # 求分割损失和超分辨损失的和
+        self.loss_G = self.loss_cross_entropy + self.loss_adv
+        self.loss_G.backward(retain_graph=True)
+
+    def backward_D(self):
+        self.loss_D1 = self.bce_loss(self.netModel_D1(F.softmax(self.pred1_cut, dim=1)), False) \
+                       + self.bce_loss(self.netModel_D1(F.softmax(self.pred_target1_cut, dim=1)), True)
+
+        self.loss_D2 = self.bce_loss(self.netModel_D2(F.softmax(self.pred2_cut, dim=1)), False) \
+                       + self.bce_loss(self.netModel_D2(F.softmax(self.pred_target2_cut, dim=1)), True)
+
+        self.loss_D = (self.loss_D1 + self.loss_D2) * 0.5
+        self.loss_D.backward()
+
+    def optimize_parameters(self):
+        self.forward()
+
+        # 不求判别器的梯度
+        self.set_requires_grad([self.netModel_D1, self.netModel_D2], False)
+
+        # 更新生成器的参数
+        self.optimizer.zero_grad()
+        self.backward()  # 计算生成器的参数的梯度
+        self.optimizer.step()  # 更新参数
+
+        # 可以求判别器的梯度
+        self.set_requires_grad([self.netModel_D1, self.netModel_D2], True)
+        self.optimizer_D.zero_grad()
+        self.backward_D()  # 计算判别器的梯度
+        self.optimizer_D.step()  # update weights
+
--- models/networks.py
+++ models/networks.py
@@ -737,3 +737,150 @@ def deeplabv2(is_restore_from_imagenet=True, resnet_weight_path="./resnetweight/
             new_state_dict[k]=v
         model.load_state_dict(new_state_dict, strict=False) #, strict=False
     return model
+
+################################################
+# adaptsegnet model
+################################################
+class Classifier_Module_Mul(nn.Module):
+    def __init__(self, inplanes, dilation_series, padding_series, num_classes):
+        super(Classifier_Module_Mul, self).__init__()
+        self.conv2d_list = nn.ModuleList()
+        for dilation, padding in zip(dilation_series, padding_series):
+            self.conv2d_list.append(
+                nn.Conv2d(inplanes, num_classes, kernel_size=3, stride=1, padding=padding, dilation=dilation, bias=True))
+
+        for m in self.conv2d_list:
+            m.weight.data.normal_(0, 0.01)
+
+    def forward(self, x):
+        out = self.conv2d_list[0](x)
+        for i in range(len(self.conv2d_list) - 1):
+            out += self.conv2d_list[i + 1](x)
+            return out
+class ResNetMulti(nn.Module):
+    def __init__(self, block, layers, num_classes):
+        self.inplanes = 64
+        super(ResNetMulti, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64, affine=affine_par)
+        for i in self.bn1.parameters():
+            i.requires_grad = False
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1, ceil_mode=True)  # change
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=1, dilation=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=1, dilation=4)
+        self.layer5 = self._make_pred_layer(Classifier_Module_Mul, 1024, [6, 12, 18, 24], [6, 12, 18, 24], num_classes)
+        self.layer6 = self._make_pred_layer(Classifier_Module_Mul, 2048, [6, 12, 18, 24], [6, 12, 18, 24], num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, 0.01)
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+                #        for i in m.parameters():
+                #            i.requires_grad = False
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilation=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion or dilation == 2 or dilation == 4:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion, affine=affine_par))
+        for i in downsample._modules['1'].parameters():
+            i.requires_grad = False
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, dilation=dilation, downsample=downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes, dilation=dilation))
+
+        return nn.Sequential(*layers)
+
+    def _make_pred_layer(self, block, inplanes, dilation_series, padding_series, num_classes):
+        return block(inplanes, dilation_series, padding_series, num_classes)
+
+    def forward(self, x):
+        _, _, h, w = x.size()
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+
+        x = self.layer3(x)
+        x1 = self.layer5(x)
+
+        x2 = self.layer4(x)
+        x2 = self.layer6(x2)
+        x1 = nn.functional.interpolate(x1, size=(h, w), mode='bilinear')
+        x2 = nn.functional.interpolate(x2, size=(h, w), mode='bilinear')
+
+        return x1, x2
+
+    def get_1x_lr_params_NOscale(self):
+        """
+        This generator returns all the parameters of the net except for
+        the last classification layer. Note that for each batchnorm layer,
+        requires_grad is set to False in deeplab_resnet.py, therefore this function does not return
+        any batchnorm parameter
+        """
+        b = []
+
+        b.append(self.conv1)
+        b.append(self.bn1)
+        b.append(self.layer1)
+        b.append(self.layer2)
+        b.append(self.layer3)
+        b.append(self.layer4)
+
+        for i in range(len(b)):
+            for j in b[i].modules():
+                jj = 0
+                for k in j.parameters():
+                    jj += 1
+                    if k.requires_grad:
+                        yield k
+
+    def get_10x_lr_params(self):
+        """
+        This generator returns all the parameters for the last layer of the net,
+        which does the classification of pixel into classes
+        """
+        b = []
+        b.append(self.layer5.parameters())
+        b.append(self.layer6.parameters())
+
+        for j in range(len(b)):
+            for i in b[j]:
+                yield i
+
+    def optim_parameters(self, opt):
+        return [{'params': self.get_1x_lr_params_NOscale(), 'lr': opt.lr},
+                {'params': self.get_10x_lr_params(), 'lr': 10 * opt.lr}]
+def AdaptSegnet(is_restore_from_imagenet=True, resnet_weight_path="./resnetweight/",  num_classes=2, gpu_ids=[]):
+    model = ResNetMulti(Bottleneck, [3, 4, 23, 3], num_classes)
+    if len(gpu_ids) > 0:
+        assert(torch.cuda.is_available())
+        model.to(gpu_ids[0])
+        model = torch.nn.DataParallel(model, gpu_ids)  # multi-GPUs
+    if is_restore_from_imagenet:
+        print("loading pretrained model (resnet101)")
+        state_dict = load_state_dict_from_url(model_urls['resnet101'], model_dir=resnet_weight_path)
+        new_state_dict = OrderedDict()
+        # 修改 key，没有module字段则需要不上，如果有，则需要修改为 module.features
+        for k, v in state_dict.items():
+            if 'module' not in k:
+                k = 'module.'+k
+            else:
+                k = k.replace('features.module.', 'module.features.')
+            new_state_dict[k]=v
+        model.load_state_dict(new_state_dict, strict=False) #, strict=False
+    return model
+
