72878f64aac44814a19a85bbbc9d9529462dfe9c
2819786276@qq.com <woai13920884917>
2020-04-03 10:36:27 +0800

    add create fakeB dataset code

---
+++ convert_cyclegan.py
@@ -0,0 +1,52 @@
+﻿from options.test_options import TestOptions
+from data import create_dataset
+from models import create_model
+from util import util
+from PIL import Image as m
+import numpy as np
+import cv2
+import os
+
+
+if __name__ == '__main__':
+    # 加载设置
+    opt = TestOptions().parse()
+    opt.batch_size = 1
+    # 加载数据集
+    dataset = create_dataset(opt)
+    dataset_size = len(dataset)
+    print('The number of training images = %d' % dataset_size)
+
+    # 创建模型
+    model = create_model(opt)
+
+    # 恢复权重
+    model.setup(opt)
+    model.eval()
+    output_dir = os.path.join(opt.dataroot, "fakeA_ltol")
+    img_dir = os.path.join(output_dir, "images")
+    label_dir = os.path.join(output_dir, "labels")
+    util.mkdirs([output_dir,img_dir,label_dir])
+    for i, data in enumerate(dataset):
+        image_name_A = data["img_path"][0].split("/")[-1]
+        image_name_B = data["label_path"][0].split("/")[-1]
+        if image_name_A.split(".")[0][5:] != image_name_B.split(".")[0][5:]:
+            print(image_name_A + "is not" + image_name_B)
+            break
+        model.set_input(data)
+        model.forward()
+        # result data(image label)
+        fake_B = model.fake_B
+        imageB_np = util.tensor2im(fake_B)[...,::-1]
+        label_data = np.squeeze(data["label"].numpy())
+
+        # result_paths(image, label)
+
+        image_out = output_dir + "/images/" + image_name_A
+        label_out = output_dir + "/labels/" + image_name_B
+        # save images
+        cv2.imwrite(image_out, imageB_np)
+        cv2.imwrite(label_out, label_data)
+        print(i + 1)
+
+
---
+++ data/convert_dataset.py
@@ -0,0 +1,88 @@
+﻿from data.base_dataset import BaseDataset
+from data.image_folder import make_dataset
+import torchvision.transforms as transforms
+from PIL import Image as m
+import torch
+import numpy as np
+import torchvision.transforms.functional as TF
+import random
+
+def transform(image, mask, opt):
+
+    if not opt.no_crop:
+        # Random crop
+        i, j, h, w = transforms.RandomCrop.get_params(
+            image, output_size=(opt.crop_size, opt.crop_size))
+        image = TF.crop(image, i, j, h, w)
+        mask = TF.crop(mask, i, j, h, w)
+
+    mask = np.array(mask).astype(np.long)
+    nomal_fun_image = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+
+    # Transform to tensor
+    image = TF.to_tensor(image)
+    image = nomal_fun_image(image)
+    mask = TF.to_tensor(mask)
+
+    return image, mask
+
+class ConvertDataset(BaseDataset):
+    """load train and val for segmentation network
+    """
+
+    @staticmethod
+    def modify_commandline_options(parser, is_train):
+        """Add new dataset-specific options, and rewrite default values for existing options.
+
+        :param parser: -- original option parser
+        :param is_train: -- whether training phase or test phase. You can use this flag to add training-specific or test-specific options.
+        :return: the modified parser.
+        """
+        parser.add_argument('--no_crop',  type=bool, default=True,
+                            help='crop the A and B according to the special datasets params  [crop | none],')
+        parser.add_argument('--crop_size', type=int, default=300, help='crop to this size')
+        parser.add_argument('--phase', type=str, default='trainA', help='train, val, test, etc  for the directory name')
+        parser.add_argument('--max_dataset_size', type=int, default=float("inf"),
+                            help='Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
+        return parser
+    def __init__(self, opt):
+        BaseDataset.__init__(self, opt)
+        self.dir_images = opt.dataroot + "/" + opt.phase + '/images'  # create a path '/trainA/images/*.png'
+        self.dir_labels = opt.dataroot + "/" + opt.phase + '/labels'  # labels path
+
+        self.images_paths = sorted(
+            make_dataset(self.dir_images, opt.max_dataset_size))  # load images from '/images/*.png'
+        self.labels_paths = sorted(
+            make_dataset(self.dir_labels, opt.max_dataset_size))  # load images from '/labels/*.png'
+        self.size = len(self.images_paths)
+
+    def __getitem__(self, index):
+        """Return a data point and its metadata information.
+
+        Parameters:
+            index (int)      -- a random integer for data indexing
+
+        Returns a dictionary that contains A, B, A_paths and B_paths
+            A (tensor)       -- an image in the input domain
+            B (tensor)       -- its corresponding image in the target domain
+            A_paths (str)    -- image paths
+            B_paths (str)    -- image paths
+        """
+        # read the image and corresponding to the label
+        image_path = self.images_paths[index % self.size]
+        label_path = self.labels_paths[index % self.size]
+
+        img = m.open(image_path).convert('RGB')  
+        label = m.open(label_path).convert('L')  
+        img, label = transform(img, label, self.opt)
+
+        return {'img': img, 'label': label, "img_path":image_path, "label_path": label_path}
+
+
+    def __len__(self):
+        """Return the total number of images in the dataset.
+
+        As we have two datasets with potentially different number of images,
+        we take a maximum of
+        """
+        return self.size
