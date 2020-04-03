8579e7868d84675c36aa4d2fcb46476c32a63f67
2819786276@qq.com <woai13920884917>
2020-04-03 08:18:31 +0800

    add cycle_gan model

--- options/base_options.py
+++ options/base_options.py
@@ -14,21 +14,21 @@ class BaseOptions():
         # basic parameters
         parser.add_argument('--dataroot', default='./datasets/remotesensing',
                             help='path to images (should have subfolders trainA, trainB, valB, etc)')
-        parser.add_argument('--name', type=str, default='one_class_adaptsegnet_htoh',
+        parser.add_argument('--name', type=str, default='one_class_cycle_gan_ltol',
                             help='name of the experiment. It decides where to store samples and models')
-        parser.add_argument('--gpu_ids', type=str, default='1', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
+        parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
         parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
 
         # model parameters
-        parser.add_argument('--model', type=str, default='adaptsegnet',
+        parser.add_argument('--model', type=str, default='cycle_gan',
                             help='chooses which model to use. [step1 | step2 | baseline]')
         # dataset parameters
-        parser.add_argument('--dataset_mode', type=str, default='htoh',
+        parser.add_argument('--dataset_mode', type=str, default='ltol',
                             help='chooses how datasets are loaded. [srda | single | baseline]')
         parser.add_argument('--serial_batches', type=bool, default=False,
                             help='if true, takes images in order to make batches, otherwise takes them randomly')
         parser.add_argument('--num_threads', default=2, type=int, help='# threads for loading data')
-        parser.add_argument('--batch_size', type=int, default=4, help='input batch size')
+        parser.add_argument('--batch_size', type=int, default=6, help='input batch size')
 
         # additional parameters
         parser.add_argument('--no_html', type=bool, default=False,
